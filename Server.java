c4da258bdaf12b39307e7bfde666527c59fcd877
dinhduccuong0907 <37355282+dinhduccuong0907@users.noreply.github.com>
2019-12-11 03:16:17 +0700

    Update Server.java

--- Server.java
+++ Server.java
@@ -1,3 +1,4 @@
+
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
@@ -11,117 +12,120 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class Server {
-	private static ServerSocket listener=null;
-	private static int clientNumber=0;
-	private static String IpClient=null;
-	public static void main(String args[])throws IOException{
-		try {
-			listener=new ServerSocket(12345);
-		}catch (IOException e) {
-			System.out.println(e);
-			System.exit(1);
-		}
-		
-		try {
-			while(true) {
-				clientNumber++;
-				boolean isNewServer=false;
-				System.out.println("Server is waiting to accept user...");
-				Socket socketOfServer = listener.accept();
-				System.out.println(socketOfServer.getInetAddress().getHostAddress());
-				if(clientNumber==1) {
-					isNewServer=true;
-					IpClient=socketOfServer.getInetAddress().getHostAddress();
-				}
-				ServiceThread st =new ServiceThread(socketOfServer, isNewServer);
-	            st.start();
-			}
-		}finally {
-			listener.close();
-		}
-	}
-	private static class ServiceThread extends Thread{
-		private final static String Folder="\\Share Folder\\";
-		private final static int sizeBuffered=2048;
-		
-		private Socket socketReceive;
-		private boolean isNewServer=false;
-		private BufferedWriter bWrite;
-		private Scanner sc;
-		
-		public ServiceThread(Socket socketOfServer,boolean statusServer) {
-			this.socketReceive=socketOfServer;
-			this.isNewServer=statusServer;
-		}
-		@Override
-		public void run() {
-                    try {
-                        DataOutputStream dos = new DataOutputStream(socketReceive.getOutputStream());
-                        bWrite = new BufferedWriter(new OutputStreamWriter(this.socketReceive.getOutputStream()));;
-                            if(isNewServer==true) {
-                                sc = new Scanner(System.in);
-                                System.out.println("name file:");
-                                String file =sc.nextLine();
-                                
-                                if(!(new File(System.getProperty("user.dir")+Folder+file)).exists()) {
-                                        System.out.println("File is not exist");
-                                }
-                                else {
-                                    System.out.println(System.getProperty("user.dir")+Folder+file);
-                                    File fileSend=new File(System.getProperty("user.dir")+Folder+file);
-                                    int fileSize = (int) fileSend.length();                     System.out.println(fileSize);
-                                    bWrite.write(file);                                         System.out.println(file);
-                                    bWrite.newLine();
-                                    bWrite.flush();
 
-                                    dos.writeInt(fileSize);
-                                    dos.flush();
-                                                                                                System.out.println("Send header done");
-                                    sendFile(this.socketReceive, fileSend);
-                                }
-                            }
-                                else {
-                                    bWrite.write(IpClient);
-                                    bWrite.newLine();
-                                    bWrite.flush();
-                            }
-                    }catch (Exception e) {
-                            System.out.println(e);
-                            e.printStackTrace();
-                    }
-		}
-		private static boolean sendFile(Socket sendToSocket,File file) throws IOException {
-                    try{
-                        DataOutputStream dos = new DataOutputStream(sendToSocket.getOutputStream());
-                            FileInputStream fis = new FileInputStream(file);
-                            byte[] data = new byte[sizeBuffered];
-                            
-                                                                                System.out.println("Start transfer file...");
-                                                                                
-                            int fileSize = (int ) file.length(), current = 0;
-                            int byteRead;
-                            do{
-                                byteRead = fis.read(data);
-                                dos.write(data, 0, byteRead);
-                                dos.flush();
-                                if(byteRead >= 0) current+=byteRead;
-                            }while(current != fileSize);
-                            System.out.println("ok");
-//                            while (true) {
-//                                byte[] buffer = new byte[sizeBuffered];
-//                                if(fis.read(buffer) > 0){
-//                                        dos.write(buffer);
-//                                }
-//                                else break;
-//                            }
-                                                                                System.out.println("Transfer Done");
-                            fis.close();
-                            
-                            return true;
-                    }catch (Exception e) {
-                        Logger.getLogger(ServiceThread.class.getName()).log(Level.SEVERE, null, e);
-                        return false;
-                    }
+    private static ServerSocket listener = null;
+    private static int clientNumber = 0;
+    private static String IpClient = null;
+
+    public static void main(String args[]) throws IOException {
+        try {
+            listener = new ServerSocket(12345);
+        } catch (IOException e) {
+            System.out.println(e);
+            System.exit(1);
+        }
+
+        try {
+            while (true) {
+                clientNumber++;
+                boolean isNewServer = false;
+                System.out.println("Server is waiting to accept user...");
+                Socket socketOfServer = listener.accept();
+                System.out.println(socketOfServer.getInetAddress().getHostAddress());
+                if (clientNumber == 1) {
+                    isNewServer = true;
+                    IpClient = socketOfServer.getInetAddress().getHostAddress();
                 }
-	}
+                ServiceThread st = new ServiceThread(socketOfServer, isNewServer);
+                st.start();
+            }
+        } finally {
+            listener.close();
+        }
+    }
+
+    private static class ServiceThread extends Thread {
+
+        private final static String Folder = "\\Share Folder\\";
+        private final static int sizeBuffered = 2048;
+
+        private Socket socketReceive;
+        private boolean isNewServer = false;
+        private BufferedWriter bWrite;
+        private Scanner sc;
+
+        public ServiceThread(Socket socketOfServer, boolean statusServer) {
+            this.socketReceive = socketOfServer;
+            this.isNewServer = statusServer;
+        }
+
+        @Override
+        public void run() {
+            try {
+                DataOutputStream dos = new DataOutputStream(socketReceive.getOutputStream());
+                bWrite = new BufferedWriter(new OutputStreamWriter(this.socketReceive.getOutputStream()));;
+                if (isNewServer == true) {
+                    sc = new Scanner(System.in);
+                    System.out.println("name file:");
+                    String file = sc.nextLine();
+
+                    if (!(new File(System.getProperty("user.dir") + Folder + file)).exists()) {
+                        System.out.println("File is not exist");
+                    } else {
+                        System.out.println(System.getProperty("user.dir") + Folder + file);
+                        File fileSend = new File(System.getProperty("user.dir") + Folder + file);
+                        int fileSize = (int) fileSend.length();
+                        System.out.println(fileSize);
+                        bWrite.write(file);
+                        System.out.println(file);
+                        bWrite.newLine();
+                        bWrite.flush();
+
+                        dos.writeInt(fileSize);
+                        dos.flush();
+                        System.out.println("Send header done");
+                        sendFile(this.socketReceive, fileSend);
+                    }
+                } else {
+                    bWrite.write(IpClient);
+                    bWrite.newLine();
+                    bWrite.flush();
+                    System.out.println("Send ip client to other");
+                }
+            } catch (Exception e) {
+                System.out.println(e);
+                e.printStackTrace();
+            }
+        }
+
+        private static boolean sendFile(Socket sendToSocket, File file) throws IOException {
+            try {
+                DataOutputStream dos = new DataOutputStream(sendToSocket.getOutputStream());
+                FileInputStream fis = new FileInputStream(file);
+                byte[] data = new byte[sizeBuffered];
+
+                System.out.println("Start transfer file...");
+
+                int fileSize = (int) file.length(), current = 0;
+                int byteRead;
+                do {
+                    byteRead = fis.read(data);
+                    dos.write(data, 0, byteRead);
+                    dos.flush();
+                    if (byteRead >= 0) {
+                        current += byteRead;
+                    }
+                } while (current != fileSize);
+                System.out.println("ok");
+
+                System.out.println("Transfer Done");
+                fis.close();
+
+                return true;
+            } catch (Exception e) {
+                Logger.getLogger(ServiceThread.class.getName()).log(Level.SEVERE, null, e);
+                return false;
+            }
+        }
+    }
 }
