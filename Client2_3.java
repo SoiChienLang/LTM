d75eacd8f9fff6d91cfec854acb55a28527dee8c
dinhduccuong0907 <37355282+dinhduccuong0907@users.noreply.github.com>
2019-12-11 03:18:00 +0700

    Update and rename Client1.java to Client2_3.java

--- Client2_3.java
+++ Client2_3.java
@@ -13,27 +13,26 @@ import java.net.*;
 import java.io.*;
 import java.util.Scanner;
 
-public class Client1 {
+public class Client2_3 {
 
     /**
      * @param args the command line arguments
      */
- 
-
     public static void main(String[] args) throws IOException {
         // TODO code application logic here
         Scanner inp = new Scanner(System.in);
 
-        int port=12345;
-        
+        int port = 12345;
+
         Socket s = new Socket("127.0.0.1", port);
-        System.out.println("conneced" + s);
+        System.out.println("connected " + s.toString());
 
         DataInputStream is = new DataInputStream(s.getInputStream());
         DataOutputStream os = new DataOutputStream(s.getOutputStream());
         // nhan dia chi ip pc1;
-
-        String x = is.readUTF();
+        BufferedReader reader = new BufferedReader(new InputStreamReader(s.getInputStream()));
+//        String x = is.readUTF();
+        String x = reader.readLine();
         System.out.println("server: " + x);
 
         if (x.equals("")) {
@@ -42,29 +41,32 @@ public class Client1 {
             // dong ket noi vs server
             s.close();
             // mo socket moi ket noi voi PC1
-            int Port = 12345;
-            Socket socket = new Socket(x,Port);
-            System.out.println("conneced "+ socket);
+            int Port = 23456;
+            Socket socket = new Socket(x, Port);
+            System.out.println("conneced " + socket);
             DataInputStream dis = new DataInputStream(socket.getInputStream());
-            DataOutputStream dos = new DataOutputStream(socket.getOutputStream());
+//            DataOutputStream dos = new DataOutputStream(socket.getOutputStream());
             // nhan ten file filesize
+            InputStream is_from_other = socket.getInputStream();
             String filename = dis.readUTF();
             int fileSize = dis.readInt();
             // luu file
-            
-             byte[] data = new byte[180000];
-                    int byteRead=0;
-                    int current = 0;
-                File    file = new File("Download Folder\\" + filename);
-                FileOutputStream    fos = new FileOutputStream(file);
-                BufferedOutputStream    bos = new BufferedOutputStream(fos);
-                    do{
-                        byteRead = is.read(data);
-                        bos.write(data,0, byteRead);
-                        if(byteRead >=0) current+=byteRead;
-                        System.out.println(current + " " + byteRead);
-                    }while(current != fileSize);
-                    bos.flush();
+
+            byte[] data = new byte[180000];
+            int byteRead = 0;
+            int current = 0;
+            File file = new File("Download Folder\\" + filename);
+            FileOutputStream fos = new FileOutputStream(file);
+            BufferedOutputStream bos = new BufferedOutputStream(fos);
+            do {
+                byteRead = is_from_other.read(data);
+                bos.write(data, 0, byteRead);
+                if (byteRead >= 0) {
+                    current += byteRead;
+                }
+                System.out.println(current + " " + byteRead);
+            } while (current != fileSize);
+            bos.flush();
         }
     }
 }
